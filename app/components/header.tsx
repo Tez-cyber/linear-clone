9fc6898f88445f2b6a5b7314f0e6f68e6d7203ad
Salami Azeez <salamitopeazeez@gmail.com>
2024-09-28 17:08:07 +0100

    Added button component to components folder

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -6,13 +6,14 @@ export const Header = () => {
     return (
         <>
             {/* ==== Header ==== */}
-            <header>
-                <Container className="flex">
+            <header className="fixed top-0 left-0 w-full border-b border-transparent-white">
+                <Container className="flex h-[var(--nav-height)]">
                     <Link className="flex items-center text-md" href="/">
                         <Logo className="w-[1.8rem] h-[1.8rem] mr-4" /> Linear
                     </Link>
-                    <nav className="">
-                        <ul className="flex">
+                    {/* ========= nav links ========= */}
+                    <nav className="h-full">
+                        <ul className="flex items-center h-full [&_a]:text-sm [&_li]:ml-6">
                             <li className="">
                                 <Link href="#">Features</Link>
                             </li>
@@ -36,6 +37,11 @@ export const Header = () => {
                             </li>
                         </ul>
                     </nav>
+                    {/* ==== ===== */}
+                    <div className="ml-auto h-full flex items-center">
+                        <Link className="text-sm mr-6" href="#">Log in</Link>
+                        <Link href="#">Sign up</Link>
+                    </div>
                 </Container>
             </header>
         </>
