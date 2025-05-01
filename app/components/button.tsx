2ed99888ce30dccded1a5ea6a5d7b3d2a025bbd3
Salami Azeez <salamitopeazeez@gmail.com>
2025-05-01 14:02:37 +0100

    Modified button

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -1,8 +1,32 @@
 import Link from "next/link"
-interface ButtonProps {
+import { cva, VariantProps } from "class-variance-authority"
+
+interface ButtonProps extends VariantProps<typeof buttonClasses>{
     children: React.ReactNode
     href: string
 }
-export const Button = ({ children, href }: ButtonProps) => {
-   return <Link href={href}>{children}</Link>
+const buttonClasses = cva("", {
+    variants: {
+        variant: {
+            primary: "bg-primary-gradient",
+            secondary: "",
+            tertiary: ""
+        },
+        size: {
+            small: "text-xs",
+            medium: "text-sm",
+            large: "text-md"
+        }
+    },
+    defaultVariants: {
+        variant: "primary",
+        size: "medium"
+    }
+})
+export const Button = ({ children, href, variant, size }: ButtonProps) => {
+   return(
+    <Link className={buttonClasses({ variant, size })} href={href}>
+        {children}
+    </Link>
+   )
 }
